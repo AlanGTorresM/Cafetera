0e4231a4d221655a87092f4c721344b0ffb53ecd
scorpionalan <alan_torres_mora@comunidad.unam.mx>
2023-03-27 09:23:28 -0600

    Tarea 5

--- src/main/java/org/example/Cafetera.java
+++ src/main/java/org/example/Cafetera.java
@@ -1,2 +1,62 @@
-package org.example;public class Cafetera {
+package org.example;
+
+public class Cafetera {
+    private int agua;
+    private int cafe;
+    private int crema;
+    private int vasos;
+    boolean condicional;
+    public Cafetera(){
+        this.agua=5000;
+        this.cafe=1000;
+        this.crema=1500;
+        this.vasos=50;
+        this.condicional=true;
+    }
+    public void MostrarCantidad(){
+        System.out.println("cantidad de agua es de: "+ this.agua);
+        System.out.println("cantidad de cafe es de: "+ this.cafe);
+        System.out.println("cantidad de crema es de: "+ this.crema);
+        System.out.println("cantidad de vasos es de: "+ this.vasos);
+    }
+    public void Americano(){
+        if(this.agua>=180 && this.cafe>=15 && this.vasos!=0){
+            this.agua-=180;
+            this.cafe-=15;
+            this.vasos--;
+            System.out.println("se sirvio un americano");
+            MostrarCantidad();
+        }else{
+            System.out.println("Ya no se puede");
+            condicional= false;
+        }
+    }
+    public void Expreso(){
+        if(this.agua>=120 && this.cafe>=20 && this.vasos!=0){
+            this.agua-=120;
+            this.cafe-=20;
+            this.vasos--;
+            System.out.println("se sirvio un expreso");
+            MostrarCantidad();
+
+        }else{
+            System.out.println("Ya no se puede");
+            condicional=false;
+        }
+    }
+    public void Capuchino(){
+        if(this.agua>=100 && this.cafe>=14 && this.crema>=70 && this.vasos!=0){
+            this.agua-=100;
+            this.cafe-=14;
+            this.crema-=70;
+            this.vasos--;
+            System.out.println("se sirvio un capuchino");
+            MostrarCantidad();
+        }else{
+            System.out.println("Ya no se puede");
+            condicional=false;
+        }
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,33 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner entrada= new Scanner(System.in);
+        int x;
+        Cafetera cafetera= new Cafetera();
+        System.out.println("Bienvenido a la cafeteria" +
+                "\n Nuestro menu es Expreso, Americano y Capuchino");
+        while(cafetera.condicional==true){
+            System.out.println("si quieres un Expreso presina 1" +
+                    "\nsi quieres un americano presiona 2" +
+                    "\nsi quieres un capuchino presiona 3");
+            x= entrada.nextByte();
+            switch (x){
+                case 1:
+                    cafetera.Expreso();
+                    break;
+                case 2:
+                    cafetera.Americano();
+                    break;
+                case 3:
+                    cafetera.Capuchino();
+                    break;
+                default:
+                    System.out.println("valor no valido");
+            }
+
+        }
     }
 }
